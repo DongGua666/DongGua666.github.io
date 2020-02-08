788e28d974c690c250ef54a156ad284bf071600d
donggua <46484689+DongGua666@users.noreply.github.com>
2020-02-08 09:40:34 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<title>LOGIN</title>
+
+<?php
+$x=<input onkeydown="if(event.keyCode==13)event.keyCode=9">
+?>
+
+if ($x="10110011")
+{
+<?php $url = $_GET['donggua666.github.io/Wax.html']; 
+Header("Location:$url"); 
+?>
+}
